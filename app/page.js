487e55f9f12c7292fa43bcfd5437c33eb5b272c8
dhruvpatel99269 <142231508+dhruvpatel99269@users.noreply.github.com>
2023-09-22 19:43:37 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -66,7 +66,7 @@ export default function Home() {
 
         <header>
           <h2 className='text-gray-700 text-6xl font-semibold'>Recipes</h2>
-          <h3 className='text-2xl font-semibold'>For Ninjas</h3>
+          <h3 className='text-2xl font-semibold'>For All</h3>
         </header>
 
         <div>
